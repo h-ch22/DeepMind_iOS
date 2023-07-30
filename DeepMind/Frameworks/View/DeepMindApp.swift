39bea49fc0ad5b1dafc52b21ea4264f0931adbf5
h-ch22 <ckdwls9460@naver.com>
2023-07-30 12:08:37 +0900

    Initialize Project

---
+++ DeepMind/Frameworks/View/DeepMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepMindApp.swift
+//  DeepMind
+//
+//  Created by 하창진 on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DeepMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+![ ](imgs/ic_deepMind_h.png)</br>
+# DeepMind</br>
+### Check your psychological status with ML, based on YOLOv8<br><br>
+
+#### Introduction
